1516be5664c10b682fb909df2fc71848eac1ad20
linjiayu <linjiayu@meituan.com>
2017-05-31 19:20:46 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-/* eslint no-shadow: 0, react/prop-types: 0  */
+/* eslint no-shadow: 0, react/prop-types: 0 */
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './framework/store';
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 const App = ({ store }) =>
   (<Provider store={store}>
     <Router>
-      <Route path="/biztone" component={Biztone} />
+      <Route path="/" component={Biztone} />
     </Router>
   </Provider>);
 
